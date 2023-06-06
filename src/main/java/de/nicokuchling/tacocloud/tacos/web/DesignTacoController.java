6a41e6131767315e8dcc83893296f33c0c32894d
nicokuchling <nico.kuchling@outlook.de>
2023-06-06 11:04:18 +0200

    Chapter 2.3 completed Current Progress: 2.4 page 55

--- src/main/java/de/nicokuchling/tacocloud/tacos/web/DesignTacoController.java
+++ src/main/java/de/nicokuchling/tacocloud/tacos/web/DesignTacoController.java
@@ -1,12 +1,16 @@
 package de.nicokuchling.tacocloud.tacos.web;
 
 import de.nicokuchling.tacocloud.tacos.Ingredient;
+import de.nicokuchling.tacocloud.tacos.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -46,5 +50,18 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        // save taco creation
+
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 }
